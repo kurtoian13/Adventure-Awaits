fb33ec1c7e7d6379a8bf27cd2e39adcc7255a0a7
kurt oian <kurtoian@kurts-iMac.local>
2020-12-28 12:14:31 -0500

    Link and publish with netifly test

---
+++ build/precache-manifest.48859aed04eae3cc74f3a4be0301cc8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f337b5b2ae0e436f43d15db72789c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c2b0f2b78c34202457",
+    "url": "/static/css/main.62927a30.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c2b0f2b78c34202457",
+    "url": "/static/js/main.428688d7.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
